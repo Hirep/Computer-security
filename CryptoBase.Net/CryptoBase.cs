1de0cc356f1c6bb6fbb56b685339d8f3bf994e65
Hirep <samoilenko.d.v@yandex.com>
2015-04-03 00:54:35 +0300

    Book Cipher finished

--- CryptoBase.Net/CryptoBase.cs
+++ CryptoBase.Net/CryptoBase.cs
@@ -8,17 +8,26 @@ namespace CryptoBase
 {
     public interface ICipher
     {
-        String Encrypt();
-        String Decrypt();
+        String Encrypt(String msg);
+        String Decrypt(String msg);
     }
     
     public class BookCipher: ICipher
     {
-        private readonly String Message, Key;
+        private readonly String Key;    /* Since format allows only two digit coordinates for key elements,
+                                         * max key length must be 99*99 = 9801 characters */
         private readonly Char[,] KeyMatrix;
-        public BookCipher(String message, String key)
+        private bool validKey(String message,String key)
         {
-            this.Message = message;
+            foreach (char letter in message)
+            {
+                if (!key.Contains(letter))
+                    return false;
+            }
+            return true;
+        }
+        public BookCipher(String key)
+        {
             this.Key = key;
 
             int sizeOfKeyMatrix = Convert.ToInt32(Math.Sqrt(Key.Length))+1;    // Calc size of KeyMatr, +1 for sure
@@ -26,26 +35,30 @@ namespace CryptoBase
             int iterator = 0;
             for (int i = 0; (i < KeyMatrix.GetLength(1)) && iterator<Key.Length; i++)   // KeyMatrix initialization with Key symbols
             {
-                for (int j = 0; j < KeyMatrix.GetLength(1); j++)
+                for (int j = 0; j < KeyMatrix.GetLength(1) && iterator < Key.Length; j++)
                 {
                     KeyMatrix[i,j] = Key[iterator];
                     iterator++;
                 }
             }
         }
-        public String Encrypt()
+        public String Encrypt(String message)
         {
-            String EText = null;
-            foreach (Char letter in Message)
+            if (!validKey(message, Key))
             {
-                bool letterFound = false;
-                for (int i = 0; i < KeyMatrix.GetLength(1) && !letterFound; i++)   // KeyMatrix initialization with Key symbols
+                return "Error code alpha-one";
+            }
+            String EText = null;
+            foreach (Char letter in message)
+            {
+                bool letterFound = false;   // breaks the cycle
+                for (int i = 0; i < KeyMatrix.GetLength(1) && !letterFound; i++)
                 {
                     for (int j = 0; j < KeyMatrix.GetLength(1) && !letterFound; j++)
                     {
                         if (letter == KeyMatrix[i, j])
                         {
-                            EText += String.Format("{0:00}{1:00}", i, j);
+                            EText += String.Format("{0:00}{1:00}", i, j);   // {n:00} format results bad behaviour for long keys(over9800)
                             letterFound = true;
                         }
                     }
@@ -53,13 +66,14 @@ namespace CryptoBase
             }
             return EText;
         }
-        public String Decrypt()
+        public String Decrypt(String message)
         {
             String PText = null;
-            CHeckpoint
-            // get EText string
-            // get blocks of 4 digits
-
+            for (int i = 0; i < message.Length; i += 4)  // deal with blocks of 4 digits
+            {
+                PText += KeyMatrix[Convert.ToInt32(message.Substring(i, 2)),
+                                Convert.ToInt32(message.Substring(i + 2, 2))];
+            }
             return PText;
         }
     }
