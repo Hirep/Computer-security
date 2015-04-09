4bff7da23fe591ec0eb5606add2bbfe3bdd4a827
Hirep <samoilenko.d.v@yandex.com>
2015-04-09 18:16:44 +0300

    CSlab5 GUI Project

---
+++ CSlab5_DES/CSlab5_DES/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace CSlab5_DES
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.textBoxIV = new System.Windows.Forms.TextBox();
+            this.groupBoxMode = new System.Windows.Forms.GroupBox();
+            this.radioButtonDecrypt = new System.Windows.Forms.RadioButton();
+            this.radioButtonEncrypt = new System.Windows.Forms.RadioButton();
+            this.labelKey = new System.Windows.Forms.Label();
+            this.labelIV = new System.Windows.Forms.Label();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBoxMode.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(138, 84);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(134, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(43, 6);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(100, 20);
+            this.textBoxKey.TabIndex = 1;
+            this.textBoxKey.Text = "abcdefgh";
+            this.textBoxKey.TextChanged += new System.EventHandler(this.textBoxKey_TextChanged);
+            // 
+            // textBoxIV
+            // 
+            this.textBoxIV.Location = new System.Drawing.Point(172, 6);
+            this.textBoxIV.Name = "textBoxIV";
+            this.textBoxIV.Size = new System.Drawing.Size(100, 20);
+            this.textBoxIV.TabIndex = 2;
+            this.textBoxIV.Text = "abcdefgh";
+            this.textBoxIV.TextChanged += new System.EventHandler(this.textBoxIV_TextChanged);
+            // 
+            // groupBoxMode
+            // 
+            this.groupBoxMode.Controls.Add(this.radioButtonDecrypt);
+            this.groupBoxMode.Controls.Add(this.radioButtonEncrypt);
+            this.groupBoxMode.Location = new System.Drawing.Point(15, 35);
+            this.groupBoxMode.Name = "groupBoxMode";
+            this.groupBoxMode.Size = new System.Drawing.Size(114, 72);
+            this.groupBoxMode.TabIndex = 3;
+            this.groupBoxMode.TabStop = false;
+            this.groupBoxMode.Text = "Mode";
+            // 
+            // radioButtonDecrypt
+            // 
+            this.radioButtonDecrypt.AutoSize = true;
+            this.radioButtonDecrypt.Location = new System.Drawing.Point(18, 42);
+            this.radioButtonDecrypt.Name = "radioButtonDecrypt";
+            this.radioButtonDecrypt.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonDecrypt.TabIndex = 1;
+            this.radioButtonDecrypt.TabStop = true;
+            this.radioButtonDecrypt.Text = "Decrypt";
+            this.radioButtonDecrypt.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonEncrypt
+            // 
+            this.radioButtonEncrypt.AutoSize = true;
+            this.radioButtonEncrypt.Checked = true;
+            this.radioButtonEncrypt.Location = new System.Drawing.Point(18, 19);
+            this.radioButtonEncrypt.Name = "radioButtonEncrypt";
+            this.radioButtonEncrypt.Size = new System.Drawing.Size(61, 17);
+            this.radioButtonEncrypt.TabIndex = 0;
+            this.radioButtonEncrypt.TabStop = true;
+            this.radioButtonEncrypt.Text = "Encrypt";
+            this.radioButtonEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // labelKey
+            // 
+            this.labelKey.AutoSize = true;
+            this.labelKey.Location = new System.Drawing.Point(12, 9);
+            this.labelKey.Name = "labelKey";
+            this.labelKey.Size = new System.Drawing.Size(25, 13);
+            this.labelKey.TabIndex = 4;
+            this.labelKey.Text = "Key";
+            // 
+            // labelIV
+            // 
+            this.labelIV.AutoSize = true;
+            this.labelIV.Location = new System.Drawing.Point(149, 9);
+            this.labelIV.Name = "labelIV";
+            this.labelIV.Size = new System.Drawing.Size(17, 13);
+            this.labelIV.TabIndex = 5;
+            this.labelIV.Text = "IV";
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Location = new System.Drawing.Point(141, 35);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(31, 13);
+            this.labelInput.TabIndex = 6;
+            this.labelInput.Text = "Input";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(135, 61);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(39, 13);
+            this.labelOutput.TabIndex = 7;
+            this.labelOutput.Text = "Output";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(172, 32);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxInput.TabIndex = 8;
+            this.textBoxInput.Text = "C:\\Users\\Dima\\Desktop\\in.txt";
+            this.textBoxInput.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBoxInput_MouseDown);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(172, 58);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOutput.TabIndex = 9;
+            this.textBoxOutput.Text = "C:\\Users\\Dima\\Desktop\\out.txt";
+            this.textBoxOutput.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBoxOutput_MouseDown);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 111);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(282, 22);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(282, 133);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.labelInput);
+            this.Controls.Add(this.labelIV);
+            this.Controls.Add(this.labelKey);
+            this.Controls.Add(this.groupBoxMode);
+            this.Controls.Add(this.textBoxIV);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.buttonStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "mainForm";
+            this.Text = "DES .NET";
+            this.groupBoxMode.ResumeLayout(false);
+            this.groupBoxMode.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.TextBox textBoxIV;
+        private System.Windows.Forms.GroupBox groupBoxMode;
+        private System.Windows.Forms.RadioButton radioButtonDecrypt;
+        private System.Windows.Forms.RadioButton radioButtonEncrypt;
+        private System.Windows.Forms.Label labelKey;
+        private System.Windows.Forms.Label labelIV;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ CSlab5_DES/CSlab5_DES/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using CryptoBase;
+
+namespace CSlab5_DES
+{
+    public partial class mainForm : Form
+    {
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(textBoxKey.Text))
+            {
+                MessageBox.Show("Enter key");
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(textBoxIV.Text))
+            {
+                MessageBox.Show("Enter IV");
+                return;
+            }
+            if (String.IsNullOrWhiteSpace(textBoxInput.Text))
+            {
+                MessageBox.Show("Choose input file");
+                return;
+            }
+            if (String.IsNullOrWhiteSpace(textBoxOutput.Text))
+            {
+                MessageBox.Show("Choose outputs file");
+                return;
+            }
+            String filePathInput = textBoxInput.Text;
+            String filePathOutput = textBoxOutput.Text;
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            DES Des = new DES(textBoxKey.Text, textBoxIV.Text, filePathInput,filePathOutput);
+            if (radioButtonEncrypt.Checked)
+            {
+                Des.Encrypt(filePathInput);
+            }
+            else
+            {
+                Des.Decrypt(filePathInput);
+            }
+            timer.Stop();
+            toolStripStatusLabel1.Text = "Done in "+timer.Elapsed;
+            
+        }
+
+        private void textBoxInput_MouseDown(object sender, MouseEventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result != DialogResult.OK) // Test result.
+            {
+                MessageBox.Show("Cannot open file");
+            }
+            else
+            {
+                textBoxInput.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void textBoxOutput_MouseDown(object sender, MouseEventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result != DialogResult.OK) // Test result.
+            {
+                MessageBox.Show("Cannot open file");
+            }
+            else
+            {
+                textBoxOutput.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void textBoxKey_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxKey.Text.Length > 8)
+            {
+                textBoxKey.Text = textBoxKey.Text.Remove(8);
+            }
+        }
+
+        private void textBoxIV_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxIV.Text.Length > 8)
+            {
+                textBoxIV.Text = textBoxIV.Text.Remove(8);
+            }
+        }
+
+        
+
+        
+
+        
+
+       
+
+    }
+}
